142f406ce1dbdfa3dd14ee67436af8c16de4219d
MiaTothova <21tothova@gmail.com>
2021-11-17 12:59:55 +0000

    Update name error

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BabyKick.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bellykick.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
