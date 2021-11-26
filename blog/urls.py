a2ed978dd45c7438e0dcbf3873fa2d9bed901c69
MiaTothova <21tothova@gmail.com>
2021-11-26 01:06:47 +0000

    Set up contact model functions

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
+    path('contact/', views.contact, name='contact'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
-    path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
-    path('contact.html', views.contact, name='contact')
+    path('like/<slug:slug>', views.PostLike.as_view(), name='post_like')
 ]
